ce96c7f31bdd28773c085e350457e02d1b86d819
Enzo Lai <147618090+EnzoLai@users.noreply.github.com>
2025-11-10 14:04:14 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+// or any files within the Snack
+
+
+export default function App() {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>helo</Text>
+        <Card>
+        </Card>
+        </Card>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#c0ceec',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
